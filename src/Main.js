f187567305a961683d8d8b4a18e0a9a9450804f6
Levi Porter <levi@levibrooke.com>
2019-05-08 11:14:28 -0700

    Create result component, add to Main

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import Search from './Search';
 import Map from './Map';
+import Result from './Result';
 
 class Main extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      searchQuery: ''
+    }
   }
 
   render() {
@@ -12,6 +17,15 @@ class Main extends Component {
       <main>
         <Search />
         <Map />
+        <h2>Here are the results for {this.state.searchQuery}</h2>
+        {/* error container reminder comment */}
+        <div className="column-container">
+          <Result api={'weather'} apiName={'Dark Sky API'} />
+          <Result api={'yelp'} apiName={'Yelp API'} />
+          <Result api={'events'} apiName={'Eventbrite API'} />
+          <Result api={'movies'} apiName={'Movie DB API'} />
+          <Result api={'trails'} apiName={'Hiking Project API'} />
+        </div>
       </main>
     )
   }
