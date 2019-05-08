0f6644e3eea87e955795436d98fcb4a4440666aa
Levi Porter <levi@levibrooke.com>
2019-05-08 10:24:25 -0700

    Switch to fragment in app.js wrapping header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Header from './Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Fragment>
         <Header />
-      </div>
+      </Fragment>
     );
   }
 }
