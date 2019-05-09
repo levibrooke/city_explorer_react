f9b83948324f50f5e1f9d4bc9f45c6b0fa7299a6
Levi Porter <levi@levibrooke.com>
2019-05-09 12:58:10 -0700

    Add map functionality to render when location present

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-export default (props) => (
-  <img id="map" src="" alt="Map of search query" />
-)
+const Map = (props) => {
+
+  let mapSrc = `https://maps.googleapis.com/maps/api/staticmap?center=${props.lat}%2c%20${props.long}&zoom=13&size=600x300&maptype=roadmap&key=AIzaSyA_1nx9epZie0de_8QOXTOVtdrikAs7Ye0`;
+
+  return (
+    <img id="map" src={`${mapSrc}`} alt="Map of search query" />
+  )
+};
+
+export default Map;
